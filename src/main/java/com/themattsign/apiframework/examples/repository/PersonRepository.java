6de371cd9d473d9863554b0f41188d4edcf9cb12
Matthew Middleton <mmiddleton@pssolutions.net>
2021-05-19 18:18:18 -0400

    Removed examples

--- src/main/java/com/themattsign/apiframework/examples/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.themattsign.apiframework.examples.repository;
-
-import com.themattsign.apiframework.examples.entity.PersonEntity;
-import com.themattsign.apiframework.repository.AbstractRepository;
-
-public interface PersonRepository extends AbstractRepository<PersonEntity> {
-}
